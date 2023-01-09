692b2dd69b991ade3d1ccf98dadf32b060d1259c
EricLod <eric.wiehman@lodicon.co.za>
2023-01-09 11:30:21 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "../todos.txt"
+def get_todos(filepath = "todos.txt"):
+    """
+    Help file define...
+    :param filepath:
+    :return:
+    """
+    with open(filepath) as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_arg, filepath = "todos.txt"):
+    """
+    Help file define...
+    :param filepath:
+    :return:
+    """
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
