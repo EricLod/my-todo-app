7b6b192ae69229a5501a5e1ddc78358a6faa23ec
EricLod <eric.wiehman@lodicon.co.za>
2023-01-09 14:09:44 +0200

    Code experiments

--- Home.py
+++ Home.py
@@ -12,6 +12,11 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
+st.text_input(label="",
+              placeholder="Enter new todo...",
+              on_change=add_todo,
+              key="new_todo")
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -20,7 +25,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="",
-              placeholder="Enter new todo...",
-              on_change=add_todo,
-              key="new_todo")
